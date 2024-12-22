8c84b24faec0c4d4f79579b4a30b37046e8e515d
burakugras <burakugras67@gmail.com>
2024-12-22 17:13:29 +0300

    added code for input element & refactoring

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
 const form = document.querySelector('.todo-form');
 const input = document.querySelector('.todo-input');
+const todoContainer = document.querySelector('.todo-container');
+let deleteBtns;
+
+const addHTML = (todo) => {
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const todoLeft = document.createElement('div');
+    todoLeft.classList.add('todo-left');
+
+    const todoCb = document.createElement('input');
+    todoCb.type = 'checkbox';
+    todoCb.checked = todo.isCompleted;
+    todoCb.classList.add('todo-cb');
+
+    const todoText = document.createElement('span');
+    todoText.classList.add('todo-text');
+    todoText.textContent = todo.text;
+
+    todoLeft.appendChild(todoCb);
+    todoLeft.appendChild(todoText);
+
+    const todoRight = document.createElement('div');
+    todoRight.classList.add('todo-right');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('todo-delete');
+    deleteBtn.textContent = 'Delete';
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('todo-edit');
+    editBtn.textContent = ('Edit');
+
+    todoRight.appendChild(deleteBtn);
+    todoRight.appendChild(editBtn);
+
+    todoDiv.appendChild(todoLeft);
+    todoDiv.appendChild(todoRight);
+
+    todoContainer.appendChild(todoDiv);
+}
 
 const startConf = () => {
     //start settings
-    const todos = localStorage.getItem('todos');
+    const todos = JSON.parse(localStorage.getItem('todos'));
     if (!todos) {
         localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+        todos.forEach((todo) => {
+            addHTML(todo);
+        })
+        deleteBtns = document.querySelectorAll('.todo-delete');
+        console.log(deleteBtns)
     }
 }
 
@@ -14,17 +61,27 @@ startConf();
 const addTodo = (e) => {
     e.preventDefault();
 
-    const todoText = input.value;
+    const inputVal = input.value;
 
     const todo = {
-        text: todoText,
+        text: inputVal,
         isCompleted: false,
     }
 
     const todos = JSON.parse(localStorage.getItem('todos'));
     todos.push(todo);
     localStorage.setItem('todos', JSON.stringify(todos));
-    console.log(todos);
+
+    addHTML(todo);
+
+    form.reset();
+
+}
+
+const deleteTodo = (e) => {
+    const todo = e.target.parentElement.parentElement;
+    todo.remove();
 }
 
 form.addEventListener('submit', addTodo);
+deleteBtns.forEach((btn) => btn.addEventListener('click', deleteTodo))
